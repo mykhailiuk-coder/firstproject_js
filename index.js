a7c98030caab2da1eba2982adb9b625363393f0e
mykhailiuk-coder <mykhailiuk.vitalii@chnu.edu.ua>
2025-07-21 11:59:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,238 +1,84 @@
+let generate_id = () => Math.floor(Math.random() * 1000000); //arrow function
+
 const data = {
-    readers: [],
-    articles: [],
-    comments: [],
+    tasks: [],
+    show_all_tasks: function(){ //object method
+        console.log("All tasks:");
+        let index = 1;
+        data.tasks.forEach(task => {
+            console.log(`${index}. ${task.title}, Done: ${task.isDone}`);
+            index++;
+        });
+    },
+    show_stats: function(){ //object method
+        console.log("Your statistics:");
+        let taskCount = data.tasks.length;
+        console.log(`Number of all tasks: ${taskCount}`);
+        let doneTasks = data.tasks.filter(task => task.isDone == true);
+        console.log(`Number of tasks done: ${doneTasks.length}`);
+        let productivity = Math.floor((doneTasks.length / taskCount) * 100);
+        console.log(`Percentage of tasks done: ${productivity}%`);
+    },
 };
 
-function generate_id(prefix){
-    return prefix + Math.floor(Math.random() * 1000000); 
-}
-
-function add_reader(name, phone) {
-    const reader = {
-        id: generate_id("reader#"),
-        name,
-        phone,
+function add_task(title){ //regular function
+    const task = {
+        id: generate_id(),
+        isDone: false,
+        isActive: true, 
+        title: title,
+        priority: "middle",
     };
-    data.readers.push(reader);
-    console.log(`Added reader: ${name}`);
-    return reader.id;
+    data.tasks.push(task);
+    console.log(`Added task: ${task.title}`);
 }
 
-function get_reader_by_id(id){
-    let reader = data.readers.find(reader => reader.id === id);
-    if (!reader){ 
-        console.log("Reader is not found"); 
+let get_id_by_title = function(title){ //function expression
+    let task = data.tasks.find(task => task.title == title);
+    if (!task){ 
+        console.log("Error: Task is not found"); 
         return; 
     }
-    return reader;
+    return task.id;
 }
 
-function get_reader_by_name(name){
-    reader = data.readers.find(reader => reader.name === name);
-    if (!reader){ 
-        console.log("Reader is not found"); 
+function filter_tasks(parameter){ //regular function
+    console.log(`Tasks filtered by ${parameter}`);
+    let filteredTasks;
+    if (parameter == "isDone"){
+        filteredTasks = data.tasks.filter(task => task.isDone == true);
+        filteredTasks.forEach(task => {
+            console.log(`${task.title}, Done: ${task.isDone}`);
+        });
+    }
+    else if (parameter == "isActive"){
+        filteredTasks = data.tasks.filter(task => task.isActive == true);
+        filteredTasks.forEach(task => {
+            console.log(`${task.title}, Active: ${task.isActive}`);
+        });
+    }
+    else { console.log("Error: invalid parameter"); return; }
+}
+
+function mark_as_done(id){ //regular function
+    let task = data.tasks.find(task => task.id == id);
+    if (!task){ 
+        console.log("Error: Task is not found"); 
         return; 
     }
-    return reader;
+    task.isDone = true;
+    task.isActive = false;
 }
 
-function edit_reader(id, new_name, new_phone){
-    let reader = get_reader_by_id(id);
-    if (!reader){ 
-        console.log("Reader is not found"); 
-        return; 
-    }
-    Object.assign(reader, {
-        name: new_name,
-        phone: new_phone,
-    });
-}
+let delete_task = (id) => data.tasks = data.tasks.filter(task => task.id != id); //arrow function
 
-function delete_reader(id) {
-    const reader = get_reader_by_id(id);
-    if (!reader) {
-        console.log("Reader is not found");
-        return;
-    }
-    data.readers = data.readers.filter(reader => reader.id !== id);
-    console.log(`Reader "${reader.name}" deleted`);
-}
+add_task("Meditation");
+add_task("Go to gym");
+add_task("Read for 1h");
+mark_as_done(get_id_by_title("Go to gym")); //callback
+delete_task(get_id_by_title("Read for 1h")); //callback
+add_task("Learn JS");
+filter_tasks("isDone");
 
-function add_article(title, author, year) { 
-    let article = {
-        id: generate_id("article#"),
-        title,
-        author,
-        year,
-        comments: [],
-    };
-    data.articles.push(article);
-    console.log(`Added article: ${title}`);
-    return article.id; 
-}
-
-function add_article_text(id, text){
-    let article = get_article_by_id(id);
-    if (!article){ 
-        console.log("Article is not found"); 
-        return; 
-    }
-    article.text = text;
-}
-
-function edit_article(id, new_title, new_author, new_year){
-    let article = get_article_by_id(id);
-    article.title = new_title; 
-    article.author = new_author; 
-    article.year = new_year; 
-}
-
-function get_article_by_id(id) {
-    let article = data.articles.find(article => article.id === id);
-    if (!article){ 
-        console.log("Article is not found"); 
-        return; 
-    }
-    return article;
-}
-
-function get_article_by_title(title) {
-    let article = data.articles.find(article => article.title === title);
-    if (!article){ 
-        console.log("Article is not found"); 
-        return; 
-    }
-    return article;
-}
-
-function delete_article(id){
-    let article = get_article_by_id(id);  
-    if (!article){ 
-        console.log("Article is not found"); 
-        return; 
-    }    
-    data.articles = data.articles.filter(article => article.id !== id);
-    console.log(`Article "${article.title}" deleted`);
-}
-
-function add_comment(article_id, text){
-    let article = get_article_by_id(article_id);
-    if (!article){ 
-        console.log("Article is not found"); 
-        return; 
-    }
-    let comment = {
-        id: generate_id("comment#"),
-        article,
-        text, 
-    };
-    data.comments.push(comment);
-    article.comments.push(comment); 
-    console.log(`Added comment on ${article.title}`);
-    return comment.id;
-}
-
-function edit_comment(comment_id, article_id, new_text){
-    let article = get_article_by_id(article_id); 
-    if (!article){ 
-        console.log("Article is not found"); 
-        return; 
-    }
-    let comment = get_comment_by_id(comment_id)
-    if (!comment){ 
-        console.log("Comment is not found"); 
-        return; 
-    }
-    Object.assign(comment, {
-        id: article_id, 
-        article,
-        text: new_text,
-    });
-    console.log(`Changed comment on ${article.title}`);
-}
-
-function get_comment_by_id(id){
-    let comment = data.comments.find(comment => comment.id === id);
-    if (!comment){ 
-        console.log("Comment is not found"); 
-        return; 
-    }
-    return comment;
-}
-
-function get_comment_by_text(text){
-    let comment = data.comments.find(comment => comment.text === text);    
-    if (!comment){ 
-        console.log("Comment is not found"); 
-        return; 
-    }
-    return comment;
-}
-
-function delete_comment(id){
-    let comment = get_comment_by_id(id);  
-    let article = comment.article;
-    if (!comment){ 
-        console.log("Comment is not found"); 
-        return; 
-    }     
-    data.comments = data.comments.filter(comment => comment.id !== id);
-    console.log(`Deleted comment on ${article.title}`);
-}
-
-function show_all_articles() {
-    console.log("\nAll articles:");
-    data.articles.forEach(article => {
-        if (!article){ 
-            console.log("Article is not found"); 
-            return; 
-        }
-        console.log(`"${article.title}" (${article.year}) by ${article.author}`);
-    });
-    console.log("\n");
-}
-
-function show_all_readers() {
-    console.log("\nAll readers:");
-    data.readers.forEach(reader => {
-        if (!reader){ 
-            console.log("Reader is not found"); 
-            return; 
-        }
-        console.log(`Name: ${reader.name}, Number: ${reader.phone}`);
-    });
-    console.log("\n");
-}
-
-function show_all_comments(){
-    console.log("\nAll comments:");
-    data.comments.forEach(comment => {
-        if (!comment){ 
-            console.log("Reader is not found"); 
-            return; 
-        }
-        console.log(`"${comment.text}" on ${comment.article.title}`);
-    });
-}
-
-console.log("\n");
-let VitaliiID = add_reader("Vitalii", "+380506485587");
-let MaxID = add_reader("Max", "+380505769234");
-edit_reader(VitaliiID, "John", "+380995416755");
-let HannaID = add_reader("Hanna", "+380992048705");
-delete_reader(HannaID);
-show_all_readers();
-
-let integralsID = add_article("Integral Calculus", "Isaac Newton", 1712);
-let jsID = add_article("JavaScript", "Laurence Lars Svekis", 2023);
-let cppID = add_article("Programming on C++", "Bern Straustrup", 2023);
-delete_article(cppID);
-edit_article(integralsID, "Light phenomenon", "Isaac Newton", 1708);
-show_all_articles();
-
-let light_commentID = add_comment(integralsID, "Newton was genius");
-edit_comment(light_commentID, integralsID, "Newton was a phenomenon");
-let js_commentID = add_comment(jsID, "JS is for geniuses");
-delete_comment(js_commentID);
-show_all_comments();
+data.show_all_tasks();
+data.show_stats();
